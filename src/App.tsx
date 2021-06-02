a7e3d6cdfc036c07db70c073f1cf57e696d72cbf
Alexis <amaryllisno@gmail.com>
2021-06-02 23:20:46 +0200

    feat: intro loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './sass/main.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Switch>
-          <Route path='/' exact component={Home}></Route>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
         </Switch>
       </Router>
     </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,27 +1,15 @@
-import { useEffect, useState } from 'react';
-
 import Guidlines from '../components/Guildines';
 import Heading from '../components/Heading';
 import Accordion from '../components/Accordion';
 import Nav from '../components/Nav';
 
-const Home = () => {
-  const [finished, setFinished] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setFinished(true);
-    }, 2800);
-    return () => clearTimeout(timer);
-  }, []);
-
+const About = () => {
   return (
     <>
-      <div className={finished ? 'intro--finished' : 'intro'}></div>
       <div className='wrapper'>
         <Guidlines />
         <div className='wrapper--inner'>
-          <Heading title='Amaryllis'></Heading>
+          <Heading title='About Me'></Heading>
           <small className='subtitle'>
             FRONTEND DEVELOPER | VISUAL DESIGNER | ARTIST | MUSICIAN
           </small>
@@ -33,4 +21,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default About;
