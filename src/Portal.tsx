3d2a9f3048c3465b60ea943b65d56ed1ae2f2bcc
Alexis <amaryllisno@gmail.com>
2021-06-08 21:57:51 +0200

    add: portal

---
+++ src/Portal.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return <div className='Portal'></div>;
+}
+
+export default App;
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Portal from './Portal';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Portal />
   </React.StrictMode>,
   document.getElementById('root')
 );
